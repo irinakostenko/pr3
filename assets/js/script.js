8d8181bef8369334278cce389abc05cfa7ac418b
Irina <ikostenko277@gmail.com>
2021-12-06 19:22:15 +0200

    pr 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,84 @@
 function Slide(index, title, background, link ) {
-   this.title = title;
-   this.background = background;
-   this.link = link;
-   this.id = "slide-" + index;
+    this.title = title;
+    this.background = background;
+    this.link = link;
+    this.id = "slide-" + index;
 }
 
 
 const Slider = {
-   current: 0,
-   slides: [],
-   initSlider: function(slides){
-       let index = 0;
-       for (let slide of slides){
-           this.slides.push(new Slide(index, slide.title, slide.background, slide.link));
-           index++;
-       }
-       this.buildSlider();
-   },
-   buildSlider: function() {
-       let sliderHTML = "";
-       for(let slide of this.slides) {
-//зверніть увагу на можливість використання ``,яка дозволяє додавати в string змінні ${}
-           sliderHTML +=
-               `<div id='${slide.id}' class='singleSlide'
+    current: 0,
+    slides: [],
+    initSlider: function(slides){
+        let index = 0;
+        for (let slide of slides){
+            this.slides.push(new Slide(index, slide.title, slide.background, slide.link));
+            index++;
+        }
+        this.buildSlider();
+    },
+    buildSlider: function() {
+        let sliderHTML = "";
+        let added = document.querySelector("div.content");
+        for(let slide of this.slides) {
+
+            sliderHTML +=
+                `<div id='${slide.id}' class='singleSlide'
            style='background-image:url(${slide.background});'>
            <div class='slideOverlay'>
            <h2>${slide.title}</h2>
            <a class='link' href='${slide.link}' target='_blank'>Open</a></div></div>`;
-       }
+            let btn = document.createElement('button');
+            btn.className = 'btn';
+            btn.id = 'button-'+slide.id;
+            btn.innerHTML = slide.id;
+            btn.onclick = function (){
+                Slider.setSlide(slide.id);
+            }
+            added.appendChild(btn);
+        }
 
-       document.getElementById("slider").innerHTML = sliderHTML;
-       document.getElementById("slide-" + this.current).style.left = 0;
-	   
+        document.getElementById("slider").innerHTML = sliderHTML;
+        document.getElementById("slide-" + this.current).style.left = 0;
+    },
+    setSlide: function (idx){
+        let index = parseInt(idx.match(/\d+/));
+        while(this.current!==index){
+            Slider.nextSlide();
+        }
+    },
+    prevSlide: function() {
+        let next;
+        if (this.current === 0 ) {
+            next = this.slides.length - 1;
+        } else {
+            next = this.current - 1;
+        }
 
-   },
-   
-   
-   prevSlide: function() {
-       let next;
-       if (this.current === 0 ) {
-           next = this.slides.length - 1;
-       } else {
-           next = this.current - 1;
-       }
+        document.getElementById("slide-" + next).style.left = "-100%";
+        document.getElementById("slide-" + this.current).style.left = 0;
 
-       document.getElementById("slide-" + next).style.left = "-100%";
-       document.getElementById("slide-" + this.current).style.left = 0;
+        document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInLeft");
+        document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutRight");
 
-       document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInLeft");
-       document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutRight");
+        this.current = next;
+    },
+    nextSlide: function(){
+        let next;
+        if (this.current === (this.slides.length - 1) ) {
+            next = 0;
+        } else {
+            next = this.current + 1;
+        }
 
-       this.current = next;
-   },
-   nextSlide: function(){
-       let next;
-       if (this.current === (this.slides.length - 1) ) {
-           next = 0;
-       } else {
-           next = this.current + 1;
-       }
+        document.getElementById("slide-" + next).style.left = "100%";
+        document.getElementById("slide-" + this.current).style.left = 0;
 
-       document.getElementById("slide-" + next).style.left = "100%";
-       document.getElementById("slide-" + this.current).style.left = 0;
+        document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInRight");
+        document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutLeft");
 
-       document.getElementById("slide-" + next).setAttribute("class", "singleSlide slideInRight");
-       document.getElementById("slide-" + this.current).setAttribute("class", "singleSlide slideOutLeft");
-
-       this.current = next;
-   },
-   showHide: function(element_id){
-    if (document.getElementById(element_id)) {
-        var obj = document.getElementById(element_id);
-        if (obj.style.display != "block") {
-            obj.style.display = "block";
-        } else obj.style.display = "none";
-    } else alert("Элемент с id: " + element_id + " не найден!");
+        this.current = next;
     }
- 
 }
 
 
